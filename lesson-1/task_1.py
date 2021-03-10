841ee6234498e86a1c4ec88717b9aa27f94e45a3
ele-droid <tselukovsky.ivan@gmail.com>
2021-03-10 23:36:44 +1100

    Выполнено одно задание

---
+++ lesson-1/task_1.py
@@ -0,0 +1,17 @@
+# Просмотреть документацию к API GitHub,
+# разобраться как вывести список репозиториев для конкретного пользователя,
+# сохранить JSON-вывод в файле *.json
+#
+
+import requests
+import json
+
+
+username = 'ele-droid'
+response = requests.get('https://api.github.com/users/' + username + '/repos')
+
+for repo in response.json():
+    print(repo['name'])
+
+with open('task_1_repo.json', 'w', encoding='utf8') as f:
+    json.dump(response.json(), f)
