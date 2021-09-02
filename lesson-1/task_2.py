f430b6467da678fb4886c0f9ff02922075158942
ele-droid <tselukovsky.ivan@gmail.com>
2021-09-02 20:41:20 +0700

    Выполнено задания урока №1

--- lesson-1/task_2.py
+++ lesson-1/task_2.py
@@ -5,4 +5,35 @@
 #
 
 import requests
-import json
+
+
+baseurl = 'https://api.hh.ru/'
+# получение токена авторизации
+headers = {
+    'User-Agent': 'MyScarpingJob',
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+data = {
+    'grand_type': 'client_credentials',
+    'client_id': '0000',
+    'client_secret': '0000'
+}
+
+response = requests.post(baseurl + 'oauth/token', data=data, headers=headers)
+response_json = response.json()
+access_token = response_json['access_token']
+
+# Получение списка вакансий по запросу Data Scientist
+headers = {
+    'User-Agent': 'MyScarpingJob',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Bearer ' + access_token
+}
+data = {
+    'text': 'Data Scientist',
+    'experience': 'noExperience',
+    'employment': 'full',
+    'schedule': 'remote'
+}
+
+response = requests.get(url=baseurl + 'vacancies', data=data, headers=headers)
